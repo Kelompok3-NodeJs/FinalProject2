ba345fce616aff69f07ec4e20036ca581cee7e91
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-08 09:28:20 +0700

    JsonWebTokenError
    Fix JsonWebToken Error
    fix Token null
    change to req.header
    Succes Put API

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,29 +1,70 @@
-const user = require('../models').User
-const { verifyToken } = require('../helpers/jwt')
+const {
+    verifyToken
+  } = require("../helpers/jwt")
+  
+  const {
+    User
+  } = require("../models")
 
-function authentication(req,res,next){
-    try{
-        const token = req.get('token')
-        const userDecoded = verifyToken(token)
-        user.findOne({
-            where: {
-                id: userDecoded.id,
-                email: userDecoded.email
-            }
-        })
-    .then(user => {
-            if(!user){
-                res.status(401).json({message: 'Invalid Token'})
-            }
-                res.locals.user = user
-                return next()
-        })
-        .catch(err => {
-            return res.status(401).json(err)
-        }) 
-    } catch(err) {
-        return res.status(401).json(err)
+
+  
+  let authentication = async(req, res, next) => {
+    try {
+   
+      const authorizationHeader = req.headers["authorization"];
+      if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+        throw {
+          code: 401,
+          message: "Token not provided or in an invalid format!,",
+        };
+      }
+      const token = authorizationHeader.replace('Bearer ', '');
+      console.log(token);
+      
+  
+      if (!token) {
+        throw {
+          code: 401,
+          message: "Token not provided!"
+        }
+      }
+
+      
+  
+      // verify token
+      const decode = verifyToken(token)
+      console.log(decode);
+  
+      const userData = await User.findOne({
+        where: {
+          id: decode.id,
+          email: decode.email
+        }
+      })
+  
+      if (!userData) {
+        console.log(!userData);
+        throw {
+          code: 401,
+          message: "User not found"
+        }
+      }
+  
+      req.UserData = {
+        id: userData.id,
+        email: userData.email,
+        username: userData.username
+      }
+
+      res.locals.userData = userData
+      next()
+  
+    } catch (error) {
+        console.log(error);
+      res.status(error.code || 500).json(error.message)
     }
-}
-
-module.exports = authentication
+  }
+  
+  module.exports = {
+    authentication
+  }
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const UserController = require('../controllers/userController');
 
 router.post('/users/register', UserController.register);
 router.post('/users/login', UserController.login);
-router.use(authentication);
 router.put('/users/:id', UserController.update,authorization);
 
 module.exports = router;
