891b73568eb03561fd208105d1c89aea84688e99
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-09 15:03:56 +0700

    Fix Autentikasi
    Add Autentication for API Put/Update Profile

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ const PhotoController = require('../controllers/photoController');
 
 router.post('/users/register', UserController.register);
 router.post('/users/login', UserController.login);
-router.put('/users/:id', UserController.update,authorization);
 router.delete('/users/:id', UserController.delete);
 router.use(authentication)
+router.put('/users/:id', UserController.update,authorization);
 router.post('/photos/', PhotoController.postPhoto);
 
 module.exports = router;
