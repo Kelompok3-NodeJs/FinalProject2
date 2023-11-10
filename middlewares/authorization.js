94ab2a4eb887959ee222cd5c70cbe9af2506e427
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-10 16:48:32 +0700

    Done Authorization

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,30 +1,25 @@
 const { User, Photo, Comment } = require('../models');
 
-async function authorization(req, res, next) {
-  const userId = res.locals.user.id; // Assuming you have the authenticated user ID in req.user
-  const reflectionId = req.params.id;
+function authorization(req, res, next) {
+    const id = req.params.id
+    const authenticatedUserId = res.locals.user.id
 
-  try {
-    const user = await User.findByPk(userId);
-
-    if (!user) {
-      return res.status(401).json({ message: 'User not found' });
-    }
-
-    // Assuming there's an association between User and other models like Photo, SocialMedia, and Comment
-    const userReflection = await user.getPhotos({
-      where: { id: reflectionId },
-    });
-
-    if (!userReflection) {
-      return res.status(404).json({ message: 'Reflection not found' });
-    }
-
-    next();
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json(err);
-  }
+    User.findByPk(id)
+    .then(result => {
+        if (!result) {
+            res.status(404).json({message: 'User Not Found'})
+        } else {
+            if (result.id === authenticatedUserId) {
+                next()
+            } else {
+                res.status(401).json({message: 'Unauthorized'})
+            }
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({message: 'Internal Server Error'})
+    })
 }
 
 async function photoAuthorization(req, res, next) {
