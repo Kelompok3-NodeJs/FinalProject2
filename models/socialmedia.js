df2926e62440fe1503938467748ca4b620d42c36
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-10 20:44:16 +0700

    Update socialmedia.js

--- models/socialmedia.js
+++ models/socialmedia.js
@@ -12,7 +12,6 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsTo(models.User)
-      this.hasMany(models.Photo)
     
     }
   }
