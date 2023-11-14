f3a58a2bcfc54ea0753fb33c13ea5c3c3dec7140
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-14 12:49:14 +0700

    Update socialmedia.js

--- models/socialmedia.js
+++ models/socialmedia.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.User)
+      this.belongsTomodels.User,{foreignKey: 'UserId'})
     
     }
   }
