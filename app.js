32dd6d250ec6d9e4ff77d02662ecf44bfa4418a9
lulujal <jaluadhi19@students.unnes.ac.id>
2023-11-15 13:45:46 +0700

    env

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
-const port = 3000;
 const router = require('./routes/index');
+const db = require('./config/db.config');
+
+db.authenticate()
+.then(() => {
+    console.log('Database connected');
+})
+.catch(err => {
+    console.log(err);
+})
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(router);
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
