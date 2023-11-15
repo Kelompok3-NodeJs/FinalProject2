c455a8c0e69cb10b6ce393b7feb2ea3f740d04cf
lulujal <jaluadhi19@students.unnes.ac.id>
2023-11-15 14:06:45 +0700

    env

--- config/db.config.js
+++ config/db.config.js
@@ -1,19 +1,14 @@
-const Sequelize = require('sequelize');
-const dotenv = require('dotenv');
-const { sequelize } = require('../models');
-
-const {
-    PGDATABASE,
-    PGUSER,
-    PGPASSWORD,
-    DATABASE_URL,
-    PGPORT
-}= process.env
-
-const db= new sequelize(DATABASE_URL,{
-    define:{
-        timestamps:false
-    }
-})
-
-module.exports = db;
+module.exports= {
+    host:"roundhouse.proxy.rlwy.net",
+    username:"postgres",
+    password:"Cg4B2223cab66b5*--2BeeBC3-ggGfG5",
+    database:"railway",
+    port:"45436",
+    dialect:"postgres",
+    pool:{
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
