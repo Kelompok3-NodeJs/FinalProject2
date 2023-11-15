53a8f4a50b9569ef41a6e5ca534cdfe1213a59eb
lulujal <jaluadhi19@students.unnes.ac.id>
2023-11-15 21:08:08 +0700

    change port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require('./routes/index');
 
 app.use(express.urlencoded({ extended: false }));
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at ${port}`)
 })
