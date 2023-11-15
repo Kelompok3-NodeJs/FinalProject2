654a1d48c520d1524d6a9ad194bb87c1f8e7b0f0
lulujal <jaluadhi19@students.unnes.ac.id>
2023-11-15 23:36:19 +0700

    fixing nodejs

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 const router = require('./routes/index');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(router);
-const port = process.env.PORT || 3000;
+
 app.listen(port, "0.0.0.0", function () {
     console.log(`This App listening on port ${port}`);
   });
