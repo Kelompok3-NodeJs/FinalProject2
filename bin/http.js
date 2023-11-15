5b5fa3e9014ee3b8a2905f59f443d654ea1772f0
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-15 19:15:11 +0700

    Update Config

--- bin/http.js
+++ bin/http.js
@@ -1,4 +1,7 @@
 require("dotenv").config()
+process.env.NODE_ENV = 'development';
+
+process.env.DATABASE_URL = 'postgres://postgres:G2AEAf-gbD66Eg415dDB2552f5aEC-Bg@monorail.proxy.rlwy.net:54208/railway';
 
 const app = require("../app")
 const PORT = process.env.PORT
