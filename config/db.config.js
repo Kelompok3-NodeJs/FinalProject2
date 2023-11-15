6cea9aa4e02e478a6fe843773412a5623f34cd4b
lulujal <jaluadhi19@students.unnes.ac.id>
2023-11-16 06:53:17 +0700

    clean unnecessary file

--- config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports= {
-    host:"viaduct.proxy.rlwy.net",
-    username:"postgres",
-    password:"4FcAE6-4Fe2G432D2EfDa*FbGfe*1GD6",
-    database:"railway",
-    port:"40490",
-    dialect:"postgres"
-};
