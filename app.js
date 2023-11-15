40a7c8f50a9451364c606b87923c4823767be875
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-15 10:03:46 +0700

    Update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
 const router = require('./routes/index');
 
 app.use(express.urlencoded({ extended: false }));
@@ -8,6 +8,5 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+
+module.exports = app
