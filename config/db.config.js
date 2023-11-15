575365f8f3895835e5b485c466a33bc5eeb4b05b
lulujal <jaluadhi19@students.unnes.ac.id>
2023-11-16 06:38:56 +0700

    db config

--- config/db.config.js
+++ config/db.config.js
@@ -4,11 +4,5 @@ module.exports= {
     password:"4FcAE6-4Fe2G432D2EfDa*FbGfe*1GD6",
     database:"railway",
     port:"40490",
-    dialect:"postgres",
-    pool:{
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
+    dialect:"postgres"
 };
