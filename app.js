4ba02d3dfbc69456e3f96298390ad3a848ebc6b9
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-23 12:00:46 +0700

    Update to nodeapp.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require("dotenv").config()
+process.env.NODE_ENV = 'development';
+process.env.DATABASE_URL = 'postgres://postgres:G2AEAf-gbD66Eg415dDB2552f5aEC-Bg@monorail.proxy.rlwy.net:54208/railway';
+
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 const router = require('./routes/index');
 
 app.use(express.urlencoded({ extended: false }));
@@ -8,5 +12,7 @@ app.use(express.json());
 
 app.use(router);
 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    });
 
-module.exports = app
