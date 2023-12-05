442489fe64b91cb0a8097cc2a8a869557a4e2db0
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-12-05 09:29:25 +0700

    still

--- __test__/photo.test.js
+++ __test__/photo.test.js
@@ -1,11 +1,24 @@
 const request = require('supertest');
 const app = require('../app');
-const { Photo, User } = require('../models');
+const { User } = require('../models');
+const { generateToken } = require('../helpers/jwt');
+const user = require('../models/user');
+let server;
 
-let authToken;
-
-beforeAll(async () => {
-  const user = await User.create({
+let createdUserId;
+let userToken;
+let userToken2;
+let UserData2;
+const updateData = {
+    email: "usertestupdate@mail.com",
+    full_name: "usertestupdate",
+    username: "usertestupdate",
+    password: "usertestupdate",
+    profile_image_url: "www.photo.com/usertest.png",
+    age: 20,
+    phone_number: "08123456710"
+}
+const userdata = {
     email: "usertest@mail.com", 
     full_name: "usertest", 
     username: "usertest", 
@@ -13,58 +26,75 @@ beforeAll(async () => {
     profile_image_url: "www.photo.com/usertest.png", 
     age: 20, 
     phone_number: "08123456789"
-  });
+}
+const userdata2 = {
+    email: "usertest2@mail.com",
+    full_name: "usertest2",
+    username: "usertest2",
+    password: "usertest2",
+    profile_image_url: "www.photo.com/usertest2.png",
+    age: 20,
+    phone_number: "08123456789"
+}
 
-  // Log in and get a token
-  const loginResponse = await request(app)
-    .post('/users/login')
-    .send({
-      email: 'usertest@mail.com', 
-      password: 'usertest', 
-    });
+beforeAll(() => {
+   server = app.listen()
+})
 
-  authToken = loginResponse.body.token;
-});
-
-
+afterAll((done) => {
+    try {
+        User.destroy({where: {}})
+        server.close(done)
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 describe('POST /photos', () => {
-  it('should create a photo and return 200 status', (done) => {
-    request(app)
-      .post('/photos')
-      .set(`Bearer ${authToken}`)
+  it('should create a photo and return 201 status', async () => {
+    // Login to get the user token
+    const loginRes = await request(app)
+      .post('/users/login')
       .send({
-        title: 'Create Photo',
-        caption: 'Testing Create Photo',
-        image_url: 'https://linkimage.com/foto.jpg',
-      })
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err); // Return here to stop further execution
-
-        done(); // Call done() only once
+        email: userdata.email,
+        password: userdata.password,
       });
+
+    // Extract the token from the login response
+    const { token } = loginRes.body;
+
+    // Make a POST request to create a photo using the obtained token for authentication
+    const photoRes = await request(app)
+      .post('/photos')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'New Photo Title',
+        caption: 'Testing Photo Creation',
+        poster_image_url: 'https://linktoimage.com/photo.jpg',
+      })
+      .expect(201);
+
+    // Assertions based on the response from creating a photo
+    expect(photoRes.body).toHaveProperty('id');
+    expect(photoRes.body).toHaveProperty('poster_image_url', 'https://linktoimage.com/photo.jpg');
+    expect(photoRes.body).toHaveProperty('title', 'New Photo Title');
+    expect(photoRes.body).toHaveProperty('caption', 'Testing Photo Creation');
   });
 
-  it('should return 401 status if not authenticated', (done) => {
-    request(app)
+  it('should return 401 status if not authenticated', async () => {
+    // Make a POST request without authentication token
+    const photoRes = await request(app)
       .post('/photos')
       .send({
-        title: 'Create Photo',
-        caption: 'Testing Create Photo',
-        image_url: 'https://linkimage.com/foto.jpg',
+        title: 'New Photo Title',
+        caption: 'Testing Photo Creation',
+        poster_image_url: 'https://linktoimage.com/photo.jpg',
       })
-      .expect(401)
-      .end((err, res) => {
-        if (err) return done(err);
+      .expect(401);
 
-        done(); // Call done() only once
-      });
+    // Assertion for unauthorized request
+    expect(photoRes.body).toHaveProperty('message', 'Unauthorized');
   });
 
-  afterAll(async () => {
-    await Photo.destroy({ where: {} });
-    await User.destroy({ where: {} });
-  });
-
+  // If needed, add more test cases for different scenarios
 });
